7ae6ca131567192353e62976fe99efce896917bd
Nnamdi <nnamdi.b@gmail.com>
2022-08-25 13:19:25 +0200

    building card component

--- Challenge.md
+++ Challenge.md
@@ -6,6 +6,9 @@ This is a small test to look at your structure, workflow and code skills as a de
 
 You are free to use whatever stack you want to complete the challenge. To submit the challenge we would like a link to a GitHub repo containing short and structured commits and some documentation of your setup.
 
+## Thoughts
+
+When I tried to do a GET call to the provided API, I wasn't getting any data back and I thought this was weird. After a few messing around in the chrome console I could see that the request was blocked by CORS policy. Which I learned you cannot get access unless you're making calls from Origin.
 
 dont know if i need to post all of the data or just status
 cant post to the api endpoint because CORS is blocking
--- components/Card.js
+++ components/Card.js
@@ -1,18 +1,13 @@
-import { Button, Flex, Heading, Input } from '@chakra-ui/react';
+import { Button, Flex, Heading, Text } from '@chakra-ui/react';
+import { useGetCards } from '..hooks/useGetCards';
 
 export default function Card() {
   return (
     <Flex height='100vh' alignItems='center' justifyContent='center'>
       <Flex direction='column' background='gray.200' p={12} rounded={6}>
-        <Heading mb={6}>Log in</Heading>
-        <Input
-          placeholder='something@email.com'
-          variant='filled'
-          mb={3}
-          type='email'
-        />
-        <Input placeholder='********' variant='filled' mb={6} type='password' />
-        <Button colorScheme='teal'>Log in</Button>
+        <Heading mb={6}>This is a card</Heading>
+        <Text>No text atm</Text>
+        <Button colorScheme='teal'>Test</Button>
       </Flex>
     </Flex>
   );
